49326775dc013dbb39777c159aaa9a916668f3a1
abhishekthakur <kumarcops@gmail.com>
2019-05-01 22:44:18 +0530

    Fixed the bug

--- Back to the future/SecondViewController.swift
+++ Back to the future/SecondViewController.swift
@@ -15,21 +15,23 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var lab3: UILabel!
     @IBOutlet weak var lab4: UILabel!
     
+    @IBAction func ButtonClicked(_ sender: Any) {
+            let utils = Utilities()
+            let year = utils.GetRandomYear()
+            lab1.text = utils.GetLeterAtIndex(str: year, location: 0)
+            lab2.text = utils.GetLeterAtIndex(str: year, location: 1)
+            lab3.text = utils.GetLeterAtIndex(str: year, location: 2)
+            lab4.text = utils.GetLeterAtIndex(str: year, location: 3)
+            
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func ButtonClicked(_ sender: Any) {
-        let utils = Utilities()
-        let year = utils.GetRandomYear()
-        lab1.text = utils.GetLeterAtIndex(str: year, location: 0)
-        lab2.text = utils.GetLeterAtIndex(str: year, location: 1)
-        lab3.text = utils.GetLeterAtIndex(str: year, location: 2)
-        lab4.text = utils.GetLeterAtIndex(str: year, location: 3)
-
-    }
-    
+  
     
 
 }
